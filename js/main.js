b3aa2b100c91c11a4692ecf534b106f09e9ee97e
Troy David Cook <www.troydavidcook@gmail.com>
2017-04-17 18:04:52 -0400

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
 console.log("up and running!");
 
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
 	console.log("User flipped " + cardOne);
+
+	var cards = ["queen","queen","king","king"];
+	var cardsInPlay = [];
+
+	var cardOne = cards[3];
+	cardsInPlay.push(cardOne);
+		console.log("User flipped queen.");
+
+	var cardTwo = cards[2];
+	cardsInPlay.push(cardTwo);
+		console.log("User flipped king.");
+
+	if(cardsInPlay.length === 2) {
+		if(cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again!");
+		}
+	}
